36f496b229cd1dc55d15a293b411809572d5f872
Arivazhagan <arivu86@gmail.com>
2013-12-03 22:33:07 +0530

    Sematics

--- Playground/js/script.js
+++ Playground/js/script.js
@@ -8,6 +8,39 @@ sematics.canvas = function(){
 
 };
 
+sematics.contextmenu = {
+incFont:function(){
+  document.getElementById("fontSizing").style.fontSize="larger";
+},
+decFont:function(){
+  document.getElementById("fontSizing").style.fontSize="smaller";
+},
+changeImage:function(){
+  var j = Math.ceil((Math.random()*39)+1);
+  document.images[0].src="https://developer.mozilla.org/media/img/promote/promobutton_mdn" + j + ".png";
+}
+};
+
+sematics.dragdrop = {
+    allowDrop:function (ev){
+        ev.preventDefault();
+    },
+    drag:function (ev){
+        ev.dataTransfer.setData("Text",ev.target.id);
+    },
+    drop:function (ev){
+        ev.preventDefault();
+        var data=ev.dataTransfer.getData("Text");
+        ev.target.appendChild(document.getElementById(data));
+    }
+};
+
 window.onload=function(){
    sematics.canvas();
+   //sematics.contextmenu.changeImage();
+   sematics.contextmenu.decFont();
+   sematics.contextmenu.incFont();
+   sematics.dragdrop.allowDrop();
+   sematics.dragdrop.drag();
+   sematics.dragdrop.drop();
 };
